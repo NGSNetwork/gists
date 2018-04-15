a360fce16c309d4e4c36779a4dea6d1ccf520b78
TheXeon <apgthexeon@gmail.com>
2018-04-15 04:01:09 -0700

    More relative stuff
    Less rubble, less trouble

--- backup_serverfiles.sh
+++ backup_serverfiles.sh
@@ -10,14 +10,16 @@
 # This uses rclone and a local directory to mash things together.
 # Very simplistic program, set what you need with variables.
 
-SERVERNAME="tf_trade"
+SERVERNAME="tf2server"
 SERVERFILESDIR="/path/to/${SERVERNAME}/serverfiles"
 GAMEDIR="tf"
-RCLONEDIR="remote:Server Backups/CHECKEM"
-
+RCLONEDIR="REMOTE:path to/folder/on/remote"
 DIRSTOBACKUP=("addons" "cfg" "materials" "models" "sound")
+WORKINGDIRECTORY="/path/to/workspace"
 
-if [-d "${SERVERNAME}/serverfiles"]; then
+cd $WORKINGDIRECTORY
+
+if [[ -d "${SERVERNAME}/serverfiles" && ! -L "${SERVERNAME}/serverfiles" ]]; then
         # We are in the actual server files, stop or we'll delete something!
         echo "${SERVERNAME}/serverfiles exists! We are in danger!"
         exit 1
@@ -26,21 +28,23 @@ fi
 echo "Working in:"
 pwd
 
-rm -rf $SERVERNAME
-mkdir $SERVERNAME
+rm -rf $WORKINGDIRECTORY/$SERVERNAME
+rm -f $WORKINGDIRECTORY/$SERVERNAME.tar.bz2
+mkdir $WORKINGDIRECTORY/$SERVERNAME
+
+echo "Copying and creating the file structure!"
 for i in ${DIRSTOBACKUP[@]}; do
-        echo "Making and setting ${SERVERNAME}/${i}!"
-        mkdir $SERVERNAME/$i
-        cp -r $SERVERFILESDIR/$GAMEDIR/$i $SERVERNAME/
+        echo "Making and setting ${WORKINGDIRECTORY}/${SERVERNAME}/${i}!"
+        mkdir $WORKINGDIRECTORY/$SERVERNAME/$i
+        cp -r $SERVERFILESDIR/$GAMEDIR/$i $WORKINGDIRECTORY/$SERVERNAME/
 done
 
 echo "Zipping up!"
-tar cf $SERVERNAME.tar $SERVERNAME/
-bzip2 $SERVERNAME.tar
+tar cjf $WORKINGDIRECTORY/$SERVERNAME.tar.bz2 $WORKINGDIRECTORY/$SERVERNAME/
 
 echo "UPLOADING! This may take a while..."
-rclone copy $SERVERNAME.tar.bz2 "$RCLONEDIR"
+rclone copy $WORKINGDIRECTORY/$SERVERNAME.tar.bz2 "$RCLONEDIR"
 
 echo "Cleaning up!"
-rm -rf $SERVERNAME
-rm -f $SERVERNAME.tar.bz2
+rm -rf $WORKINGDIRECTORY/$SERVERNAME
+rm -f $WORKINGDIRECTORY/$SERVERNAME.tar.bz2
