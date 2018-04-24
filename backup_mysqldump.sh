0aa5e33f7aeb17166af8475d983b149f17850af2
TheXeon <apgthexeon@gmail.com>
2018-04-23 23:51:52 -0700

    Make paths absolute
    No riff-raff, no trouble

--- backup_mysqldump.sh
+++ backup_mysqldump.sh
@@ -2,29 +2,32 @@
 # Backup MySQL DBs, V1
 
 # DEPENDENCIES:
-#       rclone, mysqldump, bzip2, tar, at least 2.5x the space taken up by dbs
+#       rclone, mysqldump, bzip2, at least 2.5x the space taken up by dbs
 
 # Place in a separate folder and configure the settings.
 
 # This uses rclone and a local directory to mash things together.
 # Very simplistic program, set what you need with variables.
 
-DUMPFOLDER="mysqldump"
-DUMPFILENAME="mysql_dump"
-DBTOBACKUP="db1 db2 db3 db4"
-DBUSER=""
-DBPASS=""
-RCLONEDIR="remote:Backups/Databases"
+DUMPFOLDER="dump"
+DUMPFILENAME="dumpfile"
+DBTOBACKUP="this that theother"
+WORKINGDIRECTORY="/path/to/workspace"
+DBUSER="user"
+DBPASS="password"
+RCLONEDIR="remote:path/to/backups"
+
+cd $WORKINGDIRECTORY
 
 echo "Working in:"
 pwd
-rm -rf $DUMPFOLDER
-mkdir $DUMPFOLDER
+rm -rf $WORKINGDIRECTORY/$DUMPFOLDER
+mkdir $WORKINGDIRECTORY/$DUMPFOLDER
 echo "Dumping file to ${DUMPFOLDER}/${DUMPFILENAME}.sql.bz2"
-mysqldump -u $DBUSER -p$DBPASS --databases $DBTOBACKUP | bzip2 > $DUMPFOLDER/$DUMPFILENAME.sql.bz2
+mysqldump -u $DBUSER -p$DBPASS --databases $DBTOBACKUP | bzip2 > $WORKINGDIRECTORY/$DUMPFOLDER/$DUMPFILENAME.sql.bz2
 
 echo "UPLOADING! This may take a while..."
-rclone copy $DUMPFOLDER "$RCLONEDIR"
+rclone copy $WORKINGDIRECTORY/$DUMPFOLDER "$RCLONEDIR"
 
-# Clean up after yourself
-rm -rf $DUMPFOLDER
+echo "Cleaning up!"
+rm -rf $WORKINGDIRECTORY/$DUMPFOLDER
